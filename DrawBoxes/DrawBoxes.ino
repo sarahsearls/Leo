f2f6c21514f0b72aa016b5d955a16405d34275c7
Emre Sarbak <emre.sarbak@gmail.com>
2017-11-23 16:17:14 -0600

    Added moveTo and draw functions

--- DrawBoxes/DrawBoxes.ino
+++ DrawBoxes/DrawBoxes.ino
@@ -1,3 +1,19 @@
+//TODO
+//- One dimensional array - DONE
+//- Execute the order - DONE
+//-- Write draw function - DONE
+//-- Write moveTo function - DONE
+//- Two dimensional arrays, with multiple instructions - DONE
+//- Import instructions - POSTPONED
+//- Parse instructions into a two-dimensional array - POSTPONED
+//- Add a feature so that it doesn't exceed the paper limits
+//- Move to, through roads
+//- Export shit from Python
+//- Read data from SD card
+
+//Concerns
+//- This will just keep looping through those things, I need it to do one thing only one time
+
 #include <Adafruit_MotorShield.h>
 #include <Wire.h>
 #include "utility/Adafruit_MS_PWMServoDriver.h"
@@ -9,7 +25,6 @@ Adafruit_StepperMotor *myMotor2 = AFMS.getStepper(400, 2);
 #define memory 100
 
 int sensorPin = A0;
-int sensorTime = 0;
 
 int maxX = 1200;
 int maxY = 3500;
@@ -25,6 +40,13 @@ int motorSpeed = 30;
 
 int soundMemory[memory];
 
+int instructions[][4] = {
+                          {500,100,50,50},
+                          {200,300,100,100},
+                          {0,2000,200,200}
+                          };
+int totalOrders = 3;
+
 void setup() {
   Serial.begin(9600);
   AFMS.begin();
@@ -37,29 +59,29 @@ void setup() {
 
 void loop() {
 
-  sensorTime++;
-  
-  if (corePosY > maxY) {
-    return;
-  }
-  
-  if(corePosX >= maxX || corePosX <= 0) {
-    movingRight = !movingRight;
-    down(60);
+  for(int i=0; i<totalOrders; i++){
+    draw(instructions[i][0],instructions[i][1],instructions[i][2],instructions[i][3]);  
   }
 
-  for(int i=0; i<2 ;i++){
-    int wiggleY = random(-10,10);
-    int wiggleX = random(-10,10);
-    wiggle(wiggleX, wiggleY);
-  }
-
-  movingRight ? right(10) : left(10);
+  return;
 }
 
-void wiggle(int deltaX, int deltaY){
-  deltaX>0 ? right(deltaX) : left(deltaX * -1);
-  deltaY>0 ? up(deltaY) : down(deltaY * -1);
+void draw(int x, int y, int sizeX, int sizeY){
+  moveTo(x,y);
+  right(sizeX);
+  down(sizeY);
+  left(sizeX);
+  up(sizeY);
+}
+
+void moveTo(int x, int y){
+  
+  if(x > corePosX) right(x-corePosX);
+  else left(corePosX - x);
+
+  if(y > corePosY) down(y-corePosY);
+  else up(corePosY - y);
+  
 }
 
 void up(int length) {
@@ -85,7 +107,7 @@ void left(int length) {
 void drawHorizontalLine(int length, bool leftToRight) {
   int motor = 1;
   while (length > 0){
-    listenSounds();
+    //listenSounds();
     if (motor == 1){
       myMotor1->step(1, leftToRight ? FORWARD : BACKWARD, SINGLE);
       motor = 2;
@@ -103,7 +125,7 @@ void drawHorizontalLine(int length, bool leftToRight) {
 void drawVerticalLine(int length, bool topDown) {
   int motor = 1;
   while (length > 0){
-    listenSounds();
+    //listenSounds();
     if (motor == 1){
       myMotor1->step(1, topDown ? BACKWARD : FORWARD, SINGLE);
       motor = 2;
@@ -116,53 +138,6 @@ void drawVerticalLine(int length, bool topDown) {
     length--;
   } 
 }
-
-void listenSounds(){
-  
-  int val = analogRead(sensorPin);
-
-  int minSound = 1200;
-  int maxSound = 0;
-
-  soundMemory[memory-1] = val;
-  
-  for(int i=0; i<memory-1;i++) {
-    soundMemory[i] = soundMemory[i+1];
-    if(soundMemory[i] < minSound) minSound = soundMemory[i];
-    if(soundMemory[i] > maxSound) maxSound = soundMemory[i];
-  }
-
-  soundLevel = maxSound - minSound;
-  //if(soundLevel > 1) Serial.println (soundLevel);
-  
-  //Serial.println (soundMemory[4]);
-  
-}
-
-
-void printInt(String label, int integer)
-{
-  Serial.print(label);
-  Serial.print(integer);
-  Serial.println("");
-}
-
-
-
-// Basic horizontal line algorithm: 
-void BasicHorizontalLine(){
-  if (corePosY > maxY) {
-    return;
-  }
-  
-  if(corePosX >= maxX || corePosX <= 0) {
-    movingRight = !movingRight;
-    down(50);
-  }
-  
-  movingRight ? right(2) : left(2);
-}
-
 
 
 
